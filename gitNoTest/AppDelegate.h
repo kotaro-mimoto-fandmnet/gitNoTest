d643d30ce99ae6825be8108747bc1a8e52b10c2b
Kotaro Mimoto <kotaro_mimoto@fandmnet.com>
2013-10-02 20:27:19 +0900

    Initial Commit

---
+++ gitNoTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  gitNoTest
+//
+//  Created by serori on 13/10/02.
+//  Copyright (c) 2013年 kotaro_mimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ gitNoTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitNoTest
+//
+//  Created by serori on 13/10/02.
+//  Copyright (c) 2013年 kotaro_mimoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
